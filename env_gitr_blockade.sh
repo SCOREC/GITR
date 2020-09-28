910dd1f451cc2e6dc6ecc23c2ed3fdf6f19118b1
Gopan Perumpilly <gopan.p@gmail.com>
2020-09-28 18:44:30 -0400

    blockade scripts

--- env_gitr_blockade.sh
+++ env_gitr_blockade.sh
@@ -1,16 +1,24 @@
 module purge
 module load gcc
 module load mpich
-module load cmake
+module load cmake/3.15.4-kgiql7d
 module load netcdf
 module load vim
 
 ncxx=/lore/gopan/install/build-netcdfcxx431/install
-#export NETCDF_PREFIX=$ncxx
-export NetCDF_PREFIX=$ncxx
+export NetCDF_CXX4_PREFIX=$ncxx
 export CMAKE_PREFIX_PATH=$ncxx:$CMAKE_PREFIX_PATH
 cuda=/usr/local/cuda-10.1
 export THRUST_INCLUDE_DIR=$cuda/include
 export PATH=$cuda/bin:$PATH
 export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
 
+#export  NETCDFLIB=netcdf
+#export  NETCDFLIB_CPP=libnetcdf-cxx4.so
+#libconfig=/lore/gopan/install/libconfig-1.7.2/install
+#libconf=/lore/gopan/install/build-libconfig-1.7.2/install
+libconf=/lore/gopan/install/libconfig-1.7.2-dist/install
+export LIB_CONFIG_INSTALL=$libconf
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${ncxx}/lib64/pkgconfig
+#export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${libconf}/lib64/pkgconfig
+#export CMAKE_PREFIX_PATH=${libconf}:$CMAKE_PREFIX_PATH
